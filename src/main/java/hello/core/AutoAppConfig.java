bbe973c4d76c832d07a2ed8d56711f49d82037e9
fine100 <cocochate94@gmail.com>
2023-07-04 00:41:35 +0900

    [230703] feat : 컴포넌트 스캔과 의존성, 필터, 컴포넌트 탐색

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+        basePackages = "hello.core.member",
+        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type= FilterType.ANNOTATION, classes = Configuration.class) // 컴포넌트 스캔을 할 때 빼야할 부분 설정
 )
 public class AutoAppConfig {
--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class AppConfigTest {
+public class AutoAppConfigTest {
     @Test
     void basicScan(){
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
